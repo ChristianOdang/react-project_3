7f94c2fa0e582a1571ac4b6e8ef258c53da9646c
JohnsonOdang <johnnyodanny@gmail.com>
2023-09-26 06:47:45 -0700

    react-project_2

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import Logo from './Logo'
+import Form from './Form'
+import PackingList from './PackingList'
+import Stats from './Stats'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/PackingList.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Item from "./Item";
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: true },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Charger", quantity: 5, packed: false },
+];
+
+function PackingList() {
+  return (
+    <div className='list'>
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default PackingList;
